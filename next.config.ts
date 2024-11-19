6d7c30c8ac7432a485c30fa404781e4a875d8c60
Marcus <marcus@makl.dk>
2024-11-19 21:09:23 +0100

    wip

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
     ],
     // Optional: tune these values based on your actual image sizes
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    deviceSizes: [375, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
 };
