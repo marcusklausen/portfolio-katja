73eec847a17785f580c6d7345094d888848784ca
Katjaklevring <152782968+Katjaklevring@users.noreply.github.com>
2024-12-13 09:23:25 +0100

    external link update

--- src/app/work/_components/hero/external-link.tsx
+++ src/app/work/_components/hero/external-link.tsx
@@ -5,13 +5,13 @@ import LinkWithArrow from "src/app/_components/link-with-arrow";
 import { usePathname } from "next/navigation";
 import { getCaseLink } from "src/utils";
 
-export default function ExternalLink() {
+export default function ExternalLink({title}:{title?:string}) {
   const pathname = usePathname();
   const slug = pathname.split("/").pop();
   const url = slug ? getCaseLink(slug) : "";
   return (
     <LinkWithArrow href={url} target="_blank" rel="noopener noreferrer">
-      Besøg website
+      {title || "Besøg website"}
     </LinkWithArrow>
   );
 }
--- src/app/work/duckwise/page.tsx
+++ src/app/work/duckwise/page.tsx
@@ -43,7 +43,7 @@ export default async function Page() {
           til hurtigt at skabe informativt indhold ved at kombinere forskellige
           moduler.
         </HeroDescription>
-        <ExternalLink />
+        <ExternalLink title="Konceptvideo" />
 
         <ImageRow
           className="mt-16"
